e55dee41422f95e1db48d7cd0009b451cc3292e7
Abdllrhman Alaa <abdllrhmanalaamusic@gmail.com>
2025-07-29 14:26:20 +0300

    Initial commit for 2Top API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use('/api/products', require('./routes/products'));
+app.use('/api/categories', require('./routes/categories'));
+app.use('/api/subcategories', require('./routes/subcategories'));
+app.use('/api/brands', require('./routes/brands'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/wishlist', require('./routes/wishlist'));
+app.use('/api/user-addresses', require('./routes/userAddresses'));
+app.use('/api/cart', require('./routes/cart'));
+app.use('/api/orders', require('./routes/orders'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
